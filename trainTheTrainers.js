f27159f164d93f35dd6559ee276e38106a63fdf2
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-04-29 18:35:39 +0200

    Add files via upload

---
+++ trainTheTrainers.js
@@ -0,0 +1,29 @@
+function trainTheTrainers(input) {
+    let index = 0;
+    let people = Number(input[index++]);
+    let command = input[index++];
+    let sumOfGrade = 0;
+    let counter = 0;
+
+    while (command !== "Finish") {
+
+        let name = command
+        let tempSumOfGrade = 0;
+
+
+        for (let i = 0; i < people; i++) {
+            counter++;
+            let grade = Number(input[index++]);
+            tempSumOfGrade += grade;
+            sumOfGrade += grade;
+        }
+        let avgTempGrade = tempSumOfGrade / people;
+        console.log(`${name} - ${avgTempGrade.toFixed(2)}.`);
+
+
+        command = input[index++];
+    }
+    let avgGrade = sumOfGrade / counter;
+    console.log(`Student's final assessment is ${avgGrade.toFixed(2)}.`);
+}
+trainTheTrainers(["2", "While-Loop", "6.00", "5.50", "For-Loop", "5.84", "5.66", "Finish"])
